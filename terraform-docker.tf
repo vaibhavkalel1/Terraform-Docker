91709a48278867ef9104252c8919ba045f117c8c
vaibhavkalel1 <110541903+vaibhavkalel1@users.noreply.github.com>
2024-03-04 11:18:50 +0530

    Create terraform-docker.tf

---
+++ terraform-docker.tf
@@ -0,0 +1,15 @@
+provider "docker" {}
+
+resource "docker_image" "tf_docker_image" {
+ name         = "tf_docker_image:latest"
+ keep_locally = false
+}
+
+resource "docker_container" "tf_docker_image" {
+ image = docker_image.tf_docker_image.latest
+ name  = "terraform_docker_container"
+ ports {
+   internal = 80
+   external = 8000
+ }
+}
