29b800939abedfcf205b6a1726668f4399a32fbf
vaibhavkalel1 <110541903+vaibhavkalel1@users.noreply.github.com>
2024-03-04 12:09:18 +0530

    Update terraform-docker.tf

--- terraform-docker.tf
+++ terraform-docker.tf
@@ -1,18 +1,22 @@
-provider "docker" {
-  source = "kreuzwerker/docker"
-  version = "2.0.0"
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.0.0"  # Use a version constraint to specify the provider version
+    }
+  }
 }
 
 resource "docker_image" "tf_docker_image" {
- name         = "tf_docker_image:latest"
- keep_locally = false
+  name         = "tf_docker_image:latest"
+  keep_locally = false
 }
 
 resource "docker_container" "tf_docker_image" {
- image = docker_image.tf_docker_image.latest
- name  = "terraform_docker_container"
- ports {
-   internal = 80
-   external = 8000
- }
+  image = docker_image.tf_docker_image.latest
+  name  = "terraform_docker_container"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
